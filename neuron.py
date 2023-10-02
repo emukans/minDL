d891ac3af25b73d8e2039b7c508711f3da79b23c
Eduards Mukans <eduards.mukans@graip.ai>
2023-10-02 17:21:17 +0300

    Little cleanup

--- neuron.py
+++ neuron.py
@@ -1,19 +1,22 @@
-from random import random, uniform, seed
+import math
+from random import uniform, seed
 from typing import Callable, List, Optional
 
 
 seed(5)
 
 
-def step(x):
+def relu(x):
     # return 1 if x >= 0 else 0
     return max(x, 0)
+    # return 1/(1 + math.exp(-x))
 
 
-def heviside_derivative(x):
+def relu_derivative(x):
     # return 1 if x == 0 else 0
     # return max(x, 0)
     return 1 if x > 0 else 0
+    # return relu(x) * (1 - relu(x))
 
 
 class Neuron:
@@ -42,9 +45,10 @@ class NeuronConnection:
 
 
 class NeuralNetwork:
-    def __init__(self, activation_function: Callable, learning_rate: float):
+    def __init__(self, activation_function: Callable, activation_function_derivative: Callable, learning_rate: float):
         self.layer_list = []
         self.activation = activation_function
+        self.activation_derivative = activation_function_derivative
         self.learning_rate = learning_rate
 
     def stack(self, neuron_list: List[Neuron], weight_list: List[float] = None):
@@ -58,20 +62,6 @@ class NeuralNetwork:
             for prev_neuron in self.layer_list[-2]:  # type: Neuron
                 for curr_neuron in neuron_list:  # type: Neuron
                     curr_neuron.connect(prev_neuron, next(weight_list))
-
-    def fit(self, data, iteration_count):
-        for i in range(iteration_count):
-            total_loss = 0
-            for X, y in data:
-                pred = self.forward(X)
-
-                loss = compute_loss(y, pred)
-                total_loss += loss
-
-                self.backprop(y)
-
-            if i % 1000 == 0:
-                print(f'Loss: {total_loss}')
 
     def forward(self, inputs):
         if not len(self.layer_list):
@@ -88,24 +78,35 @@ class NeuralNetwork:
 
     def backprop(self, y):
         error_list = [(self.activation(self.layer_list[-1][0].value) - y)]
-        for i, layer in reversed(list(enumerate(self.layer_list))):
+        for layer in reversed(self.layer_list[1:]):
             updated_error_list = []
-            for j, neuron in enumerate(layer):
-                if not len(neuron.connection_list):
-                    continue
-
+            for neuron, prediction_error in zip(layer, error_list):
                 for connection in neuron.connection_list:
-                    k = list(self.layer_list[i - 1]).index(connection.neuron)
-                    delta = error_list[j] * connection.weight * heviside_derivative(self.layer_list[i - 1][k].value)
+                    delta = prediction_error * connection.weight * self.activation_derivative(connection.neuron.value)
                     updated_error_list.append(delta)
-                    connection.weight -= self.learning_rate * error_list[j] * self.activation(self.layer_list[i - 1][k].value)
+                    connection.weight -= self.learning_rate * prediction_error * self.activation(connection.neuron.value)
 
-                neuron.bias -= self.learning_rate * error_list[j]
+                neuron.bias -= self.learning_rate * prediction_error
                 error_list = updated_error_list
+
+    def fit(self, data, iteration_count):
+        for i in range(iteration_count):
+            total_loss = 0
+            for X, y in data:
+                pred = self.forward(X)
+
+                loss = compute_loss(y, pred)
+                total_loss += loss
+
+                self.backprop(y)
+
+            if i % 1000 == 0:
+                print(f'Loss: {total_loss}')
 
 
 nn = NeuralNetwork(
-    activation_function=step,
+    activation_function=relu,
+    activation_function_derivative=relu_derivative,
     learning_rate=0.01
 )
 
